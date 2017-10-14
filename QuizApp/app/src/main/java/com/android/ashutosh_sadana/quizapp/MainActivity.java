334be98452a505365ac618f5ae3f77961166bf3b
Ashutosh <ashutoshsadana@gmail.com>
2017-10-15 02:24:56 +0530

    changed ID's and grading logic

--- QuizApp/app/src/main/java/com/android/ashutosh_sadana/quizapp/MainActivity.java
+++ QuizApp/app/src/main/java/com/android/ashutosh_sadana/quizapp/MainActivity.java
@@ -9,14 +9,13 @@ import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    final int Q1_ANSWER = R.id.question_1_third_logo_radio;
-    final int Q2_ANSWER = R.id.question_2_second_logo_radio;
-    final String Q3_ANSWER = "taj mahal";
-    final int Q4_ANSWER = R.id.question_4_first_logo_radio;
+    final int ANS_1 = R.id.ques1_radio3;
+    final int ANS_2 = R.id.ques2_radio2;
+    final String ANS_3 = "taj mahal";
+    final int ANS_4 = R.id.ques4_radio1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,102 +23,49 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void checkQuiz(View v) {
-        ArrayList<String> incorrectAnswersList = new ArrayList<String>();
+    public void submitQuiz(View v) {
 
-        int numberOfQuestionsCorrect = 0;
 
-        if (checkQuestion1()) {
-            numberOfQuestionsCorrect++;
-        } else {
-            incorrectAnswersList.add("Question 1");
-        }
+        int correctAnswers = 0;
 
-        if (checkQuestion2()) {
-            numberOfQuestionsCorrect++;
-        } else {
-            incorrectAnswersList.add("Question 2");
-        }
-
-        if (checkQuestion3()) {
-            numberOfQuestionsCorrect++;
-        } else {
-            incorrectAnswersList.add("Question 3");
-        }
-
-        if (checkQuestion4()) {
-            numberOfQuestionsCorrect++;
-        } else {
-            incorrectAnswersList.add("Question 4");
-        }
-
-        if (checkQuestion5()) {
-            numberOfQuestionsCorrect++;
-        } else {
-            incorrectAnswersList.add("Question 5");
+        RadioGroup rg = (RadioGroup) findViewById(R.id.ques1_radio_grp);
+        if( rg.getCheckedRadioButtonId() == ANS_1 ) {
+            correctAnswers++;
         }
 
 
-        StringBuilder sb = new StringBuilder();
-        for (String s : incorrectAnswersList) {
-            sb.append(s);
-            sb.append("\n");
+
+        RadioGroup rg2 = (RadioGroup) findViewById(R.id.ques2_radio_grp);
+
+        if( rg2.getCheckedRadioButtonId() == ANS_2 ) {
+            correctAnswers++;
+        }
+
+        EditText et = (EditText)findViewById(R.id.ques3_edit_text);
+        if (et.getText().toString().equalsIgnoreCase(ANS_3)) {
+            correctAnswers++;
+        }
+
+        RadioGroup rg4 = (RadioGroup) findViewById(R.id.ques_4_radio_grp);
+
+        if( rg4.getCheckedRadioButtonId() == ANS_4) {
+            correctAnswers++;
+        }
+
+        CheckBox c1 = (CheckBox) findViewById(R.id.ques5_checkbox1);
+        CheckBox c2 = (CheckBox) findViewById(R.id.ques5_checkbox2);
+        CheckBox c3 = (CheckBox) findViewById(R.id.ques5_checkbox3);
+        if (c1.isChecked() && c3.isChecked() && !c2.isChecked()) {
+            correctAnswers++;
         }
 
         Context context = getApplicationContext();
-        CharSequence text = "You got " + numberOfQuestionsCorrect + "/5 answers right.\n\nRecheck the following:\n" + sb.toString();
+        CharSequence text = "You got " + correctAnswers + "/5 answers right.";
         int duration = Toast.LENGTH_SHORT;
 
         Toast toast = Toast.makeText(context, text, duration);
         toast.show();
 
-    }
-    private boolean checkQuestion1() {
-        RadioGroup rg = (RadioGroup) findViewById(R.id.question_1_radio_group);
-
-        if( rg.getCheckedRadioButtonId() == Q1_ANSWER ) {
-            return true;
-        }
-
-        return false;
-    }
-
-    private boolean checkQuestion2() {
-        RadioGroup rg = (RadioGroup) findViewById(R.id.question_2_radio_group);
-
-        if( rg.getCheckedRadioButtonId() == Q2_ANSWER ) {
-            return true;
-        }
-
-        return false;
-    }
-
-    private boolean checkQuestion3() {
-        EditText et = (EditText)findViewById(R.id.question_3_edit_text);
-
-        return et.getText().toString().equalsIgnoreCase(Q3_ANSWER);
-    }
-
-    private boolean checkQuestion4() {
-        RadioGroup rg = (RadioGroup) findViewById(R.id.question_4_radio_group);
-
-        if( rg.getCheckedRadioButtonId() == Q4_ANSWER ) {
-            return true;
-        }
-
-        return false;
-    }
-
-    private boolean checkQuestion5() {
-        CheckBox c1 = (CheckBox) findViewById(R.id.question_5_first_checkbox);
-        CheckBox c2 = (CheckBox) findViewById(R.id.question_5_second_checkbox);
-        CheckBox c3 = (CheckBox) findViewById(R.id.question_5_third_checkbox);
-
-        if (c1.isChecked() && c3.isChecked() && !c2.isChecked()) {
-            return true;
-        }
-
-        return false;
     }
 
 }
